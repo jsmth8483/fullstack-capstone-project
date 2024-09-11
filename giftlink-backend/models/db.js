90276faf0ef81e844d24235f324bda5b8e0f04cc
James Smith <jameswsmith805@gmail.com>
2024-09-11 13:12:45 -0700

    added gift api routes

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -6,23 +6,23 @@ const MongoClient = require('mongodb').MongoClient;
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
-const dbName = "giftdb";
+const dbName = 'giftdb';
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+	if (dbInstance) {
+		return dbInstance;
+	}
 
-    const client = new MongoClient(url);      
+	const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+	// Task 1: Connect to MongoDB
+	await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+	// Task 2: Connect to database giftDB and store in variable dbInstance
+	dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+	// Task 3: Return database instance
+	return dbInstance;
 }
 
 module.exports = connectToDatabase;
